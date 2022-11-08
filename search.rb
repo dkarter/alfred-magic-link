58921ff16f6b24cc9fda1c396625a70a7d4dced7
Dorian Karter <dkarter@users.noreply.github.com>
2022-11-07 22:00:01 -0600

    Initial commit

---
+++ search.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'net/http'
+require 'cgi'
+
+Encoding.default_external = Encoding::UTF_8
+
+# TODO: turn this into a class that runs itself at the bottom of the file
+
+query = ARGV[0]
+
+uri = URI('https://duckduckgo.com/')
+params = { q: "#{query} !" }
+uri.query = URI.encode_www_form(params)
+
+# get response from duckduckgo
+res = Net::HTTP.get_response(uri)
+
+# parse redirect url
+result_url = CGI.unescape(res.body.match(/.+uddg=(.+)&rut=.+/).captures[0])
+
+# get the title
+title_resp = Net::HTTP.get_response(URI(result_url))
+title = if title_resp.is_a?(Net::HTTPSuccess)
+          title_resp.body.match(%r{<title>(.*)</title>}).captures[0]
+        else
+          query
+        end
+
+out = {
+  items: [
+    {
+      title: title,
+      subtitle: result_url,
+      arg: result_url,
+      variables: {
+        title: title,
+        url: result_url,
+      },
+    },
+  ],
+}
+
+# puts out
+
+puts out.to_json
